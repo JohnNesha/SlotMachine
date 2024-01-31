26e69b1fdd52b24b8867c3b5556c3c94b42bfa47
JohnNesha <150056202+JohnNesha@users.noreply.github.com>
2024-01-30 22:08:11 -0500

    Improved Welcome Statement

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Slot Machine!");
+        Console.WriteLine("Welcome to the Slot Machine! Press the Return key to begin");
     }
 }
-
