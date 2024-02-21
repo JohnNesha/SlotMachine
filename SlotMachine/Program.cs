78dc205c19a00426fd1c2de2b4a5d7b5e0bafd1c
JohnNesha <150056202+JohnNesha@users.noreply.github.com>
2024-02-20 22:45:02 -0500

    fix #3 Converted 3 to constant

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -5,6 +5,8 @@ class Program
     public const double COST_PER_SPIN = 2.00;
     public const int MAX_NUMBER = 9;
     public const char START_SPIN = 'Y';
+    public const int VERTICAL_LINE = 3;
+    public const int HORIZONTAL_LINE = 3; 
 
     static void Main(string[] args)
     {
@@ -21,13 +23,13 @@ class Program
                 Console.WriteLine($"\nYou have entered ${playerMoney}. Let the games begin!\n");
                 break;
             }
-
+            //If player doesn't enter an amount over spin costs player will be advised to enter more money
             {
                Console.WriteLine($"You have entered ${playerMoney}. Please enter more money to begin the game.\n");
                playerMoney = double.Parse(Console.ReadLine());
             }
         }
-
+        //Player hits Y to start or spin the game 
         Console.WriteLine("Hit the 'Y' button to spin\n");
         char spinButton = char.ToUpper(Console.ReadKey().KeyChar);
 
@@ -35,43 +37,43 @@ class Program
         
         if (spinButton == START_SPIN)
         {
-            Console.WriteLine("\nSpinning...");
-            Console.WriteLine("\nSpinning...");
+          Console.WriteLine("\nSpinning...");
+          Console.WriteLine("\nSpinning...");
 
-            //Insert numbers into 2DArray using Random Numbers
-            int[,] slotNumbers = new int[3, 3];
-            Random ranNum = new Random();
+          //Insert numbers into 2DArray using Random Numbers
+          int[,] slotNumbers = new int[VERTICAL_LINE, HORIZONTAL_LINE];
+          Random ranNum = new Random();
 
-            Console.WriteLine("\n");
+         Console.WriteLine("\n");
 
             for (int i = 0; i < slotNumbers.GetLength(0); i++)
             {
-                for (int j = 0; j < slotNumbers.GetLength(1); j++)
-                {
-                    slotNumbers[i, j] = ranNum.Next(1, MAX_NUMBER + 1);
-                    Console.Write(slotNumbers[i, j] + " ");
-                }
+               for (int j = 0; j < slotNumbers.GetLength(1); j++)
+               {
+               //Numbers will display after spin 
+               slotNumbers[i, j] = ranNum.Next(1, MAX_NUMBER + 1);
+               Console.Write(slotNumbers[i, j] + " ");
+               }
 
-                Console.WriteLine("\n");
+               Console.WriteLine("\n");
 
-                if (i == 1 && slotNumbers[i, 0] == slotNumbers[i, 1] && slotNumbers[i, 1] == slotNumbers[i, 2])
-                {
-                    Console.WriteLine("You got three in a row! You win!");
-                    break;
-                }
+               if (i == 1 && slotNumbers[i, 0] == slotNumbers[i, 1] && slotNumbers[i, 1] == slotNumbers[i, 2])
+               {
+               //If there are 3 matching numbers in the center row
+               Console.WriteLine("You got three in a row! You win!");
+               break;
+               }
 
-                else
-                {
-                    Console.WriteLine("\nTry Again.\n");
-                }
-
-            }
+               else
+               {
+               Console.WriteLine("\nTry Again.\n");
+               }
+            }    
         }
-
+        //If user enters any other letter or character besides 'Y' or 'y'
         else
         {
             Console.WriteLine("\nInvalid character chosen. Please hit 'Y' to spin");
-        }
-        
+        }    
     }
 }
